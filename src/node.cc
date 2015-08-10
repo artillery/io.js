0686ab9cb97dace4784c7189be53847c1c7ab0cb
Mark Logan <mark@artillery.com>
2015-08-10 09:50:33 -0700

    Debugger support for workers.

--- src/node.cc
+++ src/node.cc
@@ -3334,7 +3334,7 @@ static void DispatchMessagesDebugAgentCallback(Environment* env) {
 }
 
 
-static void StartDebug(Environment* env, bool wait) {
+void StartDebug(Environment* env, bool wait) {
   CHECK(!debugger_running);
 
   env->debugger_agent()->set_dispatch_handler(
@@ -3349,7 +3349,7 @@ static void StartDebug(Environment* env, bool wait) {
 
 
 // Called from the main thread.
-static void EnableDebug(Environment* env) {
+void EnableDebug(Environment* env) {
   CHECK(debugger_running);
 
   // Send message to enable debug in workers
