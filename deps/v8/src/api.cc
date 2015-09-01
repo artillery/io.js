72925c4ff144f4ba0ff2a0e7b6a3015706985b9a
Mark Logan <mark@artillery.com>
2015-09-01 16:28:11 -0700

    Adjust external memory downward because v8 doesn't account for it correctly.

--- deps/v8/src/api.cc
+++ deps/v8/src/api.cc
@@ -6538,6 +6538,18 @@ v8::ArrayBuffer::Contents v8::ArrayBuffer::Externalize() {
   self->set_is_external(true);
   isolate->heap()->UnregisterArrayBuffer(isolate->heap()->InNewSpace(*self),
                                          self->backing_store());
+  auto byte_length = static_cast<size_t>(self->byte_length()->Number());
+  /*
+   * Added by artillery - adjust memory downward to prevent v8 from needless triggering
+   * mark-and-sweep cycles.
+   * See: https://code.google.com/p/v8/issues/detail?id=4409
+   * Remove this patch when
+   * https://chromium.googlesource.com/v8/v8.git/+/15a0ace533f5810bf87382decbbaf4799f6cac8e
+   * lands in io.js. Test with the repro I made, as it isn't clear to me from reading the
+   * above patch why it fixes this issue.
+   */
+  reinterpret_cast<v8::Isolate*>(isolate)->AdjustAmountOfExternalAllocatedMemory(
+      -static_cast<int64_t>(byte_length));
 
   return GetContents();
 }
