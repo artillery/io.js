nodejs/node

workers: initial implementation

src/util.cc
@@ -3,6 +3,25 @@
 
 namespace node {
 
+char* ToUtf8Value(v8::Isolate* isolate, v8::Handle<v8::Value> value) {
+  if (value.IsEmpty())
+    return nullptr;
+
+  v8::Local<v8::String> string_value = value->ToString(isolate);
+  if (string_value.IsEmpty())
+    return nullptr;
+
+  // Allocate enough space to include the null terminator
+  size_t len = StringBytes::StorageSize(string_value, UTF8) + 1;
+  char* res = static_cast<char*>(malloc(len));
+
+  const int flags =
+      v8::String::NO_NULL_TERMINATION | v8::String::REPLACE_INVALID_UTF8;
+  int length = string_value->WriteUtf8(res, len, 0, flags);
+  res[length] = '\0';
+  return res;
+}
+
 Utf8Value::Utf8Value(v8::Isolate* isolate, v8::Handle<v8::Value> value)
     : length_(0), str_(str_st_) {
   if (value.IsEmpty())

src/util.h
@@ -2,6 +2,7 @@
 #define SRC_UTIL_H_
 
 #include "v8.h"
+#include "uv.h"
 
 #include <assert.h>
 #include <stddef.h>
@@ -42,6 +43,8 @@ namespace node {
 #define CHECK_LE(a, b) CHECK((a) <= (b))
 #define CHECK_LT(a, b) CHECK((a) < (b))
 #define CHECK_NE(a, b) CHECK((a) != (b))
+
+#define STATIC_ASSERT(expression) static_assert(expression, #expression)
 
 #define UNREACHABLE() abort()
 
@@ -98,6 +101,7 @@ class ListHead {
   inline void MoveBack(ListHead* that);
   inline void PushBack(T* element);
   inline void PushFront(T* element);
+  inline void Remove(T* element);
   inline bool IsEmpty() const;
   inline T* PopFront();
   inline Iterator begin() const;
@@ -165,8 +169,13 @@ inline void Wrap(v8::Local<v8::Object> object, void* pointer);
 
 inline void ClearWrap(v8::Local<v8::Object> object);
 
+inline void BarrierInitAndWait(uv_barrier_t* barrier, int count);
+inline void BarrierWait(uv_barrier_t* barrier);
+
 template <typename TypeName>
 inline TypeName* Unwrap(v8::Local<v8::Object> object);
+
+char* ToUtf8Value(v8::Isolate* isolate, v8::Handle<v8::Value> value);
 
 class Utf8Value {
   public:

artillery/io.js

src/util.cc
@@ -3,6 +3,25 @@
 
 namespace node {
 
+char* ToUtf8Value(v8::Isolate* isolate, v8::Local<v8::Value> value) {
+  if (value.IsEmpty())
+    return nullptr;
+
+  v8::Local<v8::String> string_value = value->ToString(isolate);
+  if (string_value.IsEmpty())
+    return nullptr;
+
+  // Allocate enough space to include the null terminator
+  size_t len = StringBytes::StorageSize(string_value, UTF8) + 1;
+  char* res = static_cast<char*>(malloc(len));
+
+  const int flags =
+      v8::String::NO_NULL_TERMINATION | v8::String::REPLACE_INVALID_UTF8;
+  int length = string_value->WriteUtf8(res, len, 0, flags);
+  res[length] = '\0';
+  return res;
+}
+
 Utf8Value::Utf8Value(v8::Isolate* isolate, v8::Local<v8::Value> value)
     : length_(0), str_(str_st_) {
   if (value.IsEmpty())

src/util.h
@@ -2,6 +2,7 @@
 #define SRC_UTIL_H_
 
 #include "v8.h"
+#include "uv.h"
 
 #include <assert.h>
 #include <signal.h>
@@ -51,7 +52,9 @@ namespace node {
 #define CHECK_LT(a, b) CHECK((a) < (b))
 #define CHECK_NE(a, b) CHECK((a) != (b))
 
-#define UNREACHABLE() ABORT()
+#define STATIC_ASSERT(expression) static_assert(expression, #expression)
+
+#define UNREACHABLE() abort()
 
 // TAILQ-style intrusive list node.
 template <typename T>
@@ -106,6 +109,7 @@ class ListHead {
   inline void MoveBack(ListHead* that);
   inline void PushBack(T* element);
   inline void PushFront(T* element);
+  inline void Remove(T* element);
   inline bool IsEmpty() const;
   inline T* PopFront();
   inline Iterator begin() const;
@@ -173,8 +177,13 @@ inline void Wrap(v8::Local<v8::Object> object, void* pointer);
 
 inline void ClearWrap(v8::Local<v8::Object> object);
 
+inline void BarrierInitAndWait(uv_barrier_t* barrier, int count);
+inline void BarrierWait(uv_barrier_t* barrier);
+
 template <typename TypeName>
 inline TypeName* Unwrap(v8::Local<v8::Object> object);
+
+char* ToUtf8Value(v8::Isolate* isolate, v8::Local<v8::Value> value);
 
 class Utf8Value {
   public: